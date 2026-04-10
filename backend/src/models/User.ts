2d9cad447ed829b4987b7513138104e622d90599
Bolice1 <komezusengebolice@gmail.com>
2026-04-10 19:29:39 +0200

    User.ts update

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,4 +1,4 @@
-import mongoose, { Document, mongo, Schema } from "mongoose";
+import mongoose, { Document, Schema } from "mongoose";
 import { createEmptyTalentProfile, ITalentProfile, talentProfileSchema } from "./talentProfile";
 import { UserRole } from "../types/express";
 
