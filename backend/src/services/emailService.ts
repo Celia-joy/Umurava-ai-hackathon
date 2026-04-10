88185c2cb508722c0abdd5e4d64059286184d5a2
Bolice1 <komezusengebolice@gmail.com>
2026-04-10 16:47:15 +0200

    integration of the email service to ensure that users are inboxed about the results

--- backend/src/services/emailService.ts
+++ backend/src/services/emailService.ts
@@ -1,24 +1,3 @@
-// we want to send emails to all the users (registration.type == new and )
+import nodemailer from 'nodemailer'
 
-import nodemailer from 'nodemailer';
-export async function sendEmail(to: string, subject: string, html: string) {
-    // Create a transporter object using SMTP transport
-    const transporter = nodemailer.createTransport({
-        host: 'smtp.example.com', // Replace with your SMTP server
-        port: 587, // Replace with your SMTP port
-        secure: false, // true for 465, false for other ports
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS,
-        },
-    });
-
-    // Send mail with defined transport object
-    const info = await transporter.sendMail({
-        from: '"UmuravaAI" <process.env.EMAIL_USER>', // sender address
-        to,
-        subject,
-        html,
-    });
-
-    console.log('Message sent: %s', info.messageId); 
+const transporter = nodemailer.createTransport
